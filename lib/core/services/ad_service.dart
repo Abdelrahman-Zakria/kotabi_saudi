aa0446f372d1593025e877696dc7c0671598077f
Abdelrahman-Zakria <abdelrahmanzakria122@gmail.com>
2026-09-16 07:43:18 +0300

    1.0.1 V6

--- lib/core/services/ad_service.dart
+++ lib/core/services/ad_service.dart
@@ -114,7 +114,7 @@ class AdService {
     _appOpenAdTimer?.cancel();
     _interstitialAdTimer?.cancel();
 
-    _appOpenAdTimer = Timer.periodic(const Duration(seconds: 40), (timer) {
+    _appOpenAdTimer = Timer.periodic(const Duration(seconds: 120), (timer) {
       if (!_isAdShowing && !IapService().isAdFree) {
         dev.log('Triggering 40-second periodic App Open ad');
         showAppOpenAdIfAvailable();
@@ -123,7 +123,7 @@ class AdService {
       }
     });
 
-    _interstitialAdTimer = Timer.periodic(const Duration(seconds: 90), (timer) {
+    _interstitialAdTimer = Timer.periodic(const Duration(seconds: 240), (timer) {
       if (!_isAdShowing && !IapService().isAdFree) {
         dev.log('Triggering 90-second periodic Interstitial ad');
         showInterstitialAd(onAdDismissed: () {});
